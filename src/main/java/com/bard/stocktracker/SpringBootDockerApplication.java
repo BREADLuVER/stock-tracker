908d78b95859e23dd294cd2611cbcdfa57ef9cfb
BREADLuVER <ry2050@nyu.edu>
2024-08-25 18:55:36 -0400

    FIXED dependencies on name changes

--- src/main/java/com/bard/stocktracker/SpringBootDockerApplication.java
+++ src/main/java/com/bard/stocktracker/SpringBootDockerApplication.java
@@ -1,4 +1,4 @@
-package com.example.spring_boot_docker;
+package com.bard.stocktracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
