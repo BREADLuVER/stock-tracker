2d126885a98131efa6999d7f52d07f3fff6ab637
BREADLuVER <ry2050@nyu.edu>
2024-09-01 20:39:24 -0400

    react set up (build setup)

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-export const fetchStockData = async (symbol) => {
-    const response = await fetch(`/fetch/${symbol}`);
-    if (!response.ok) {
-      throw new Error(`Error fetching data for ${symbol}`);
-    }
-    return response.json();
-  };
-  
